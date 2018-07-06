554828cfc37c1fcd161602633630df2ce1c695dc
leduyhoang1994 <leduyhoang1994@gmail.com>
2018-07-06 15:55:51 +0700

    loading

--- wp-content/themes/kuteshop/header.php
+++ wp-content/themes/kuteshop/header.php
@@ -19,9 +19,29 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="profile" href="http://gmpg.org/xfn/11">
 		<?php wp_head(); ?>
+        <script>
+            var $ = jQuery;
+            $(window).load(function() {
+                $(".bg_load").fadeOut("slow");
+                $(".wrapper").fadeOut("slow");
+            });
+        </script>
     </head>
 
 <body <?php body_class(); ?>>
+<div class="bg_load"></div>
+<div class="wrapper">
+    <div class="inner">
+        <span>Đ</span>
+        <span>a</span>
+        <span>n</span>
+        <span>g</span>
+        <span> </span>
+        <span>T</span>
+        <span>ả</span>
+        <span>i</span>
+    </div>
+</div>
 <?php do_action( 'kuteshop_header_sticky' ); ?>
     <div class="body-overlay"></div>
     <div id="box-mobile-menu" class="box-mobile-menu full-height">
